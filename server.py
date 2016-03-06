b04b83048bf33b83367cbc2fa9c95e8dc1385535
Moez Bouhlel <bmoez.j@gmail.com>
2016-03-06 15:12:24 +0100

    test

---
+++ server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import sys
+
+port = 8080
+if len(sys.argv) == 2:
+    try:
+        port = int(sys.argv[1])
+    except:
+        pass
+server_address = ("", port)
+SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
+SimpleHTTPRequestHandler.extensions_map.update({
+    '.dat': 'application/x-ns-proxy-autoconfig',
+})
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+sa = httpd.socket.getsockname()
+print("Serving HTTP on", sa[0], "port", sa[1], "...")
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt received, exiting.")
+    httpd.server_close()
+    sys.exit(0)
